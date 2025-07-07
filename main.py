a77bb28b41b3b8ccd8796ae51ac0d0b5934b8a9f
chaoukat99 <chawkatomar@gmail.com>
2025-07-07 13:27:36 +0100

    First commit on Python Course

---
+++ main.py
@@ -0,0 +1,134 @@
+#print("hello gomycoders")
+# this is a single line comment 
+#print("Hello developers")
+#print("hello marketers")
+
+
+
+
+# Uppercase
+
+# Lowercase
+
+# CamelCase
+
+# Variables
+
+
+
+# Username="Samir"
+
+
+# userIsConnected="Saleh"
+
+
+#Type de données 
+
+
+# Opérateurs
+
+
+# Control flow 
+   # conditions
+   # methodes Prédefinis  
+   # boucles 
+   # Functions
+  
+
+
+
+
+
+# Variables
+
+
+# username="Omar"
+
+# username="AMine"
+# username="Samir"
+# print(username)
+
+
+
+# Data types
+
+# String (textes)
+
+
+
+email="email12@gmail.com"
+age=29
+city="Casablanca"
+test=False
+
+print(email,age)
+
+# Numbers 
+
+# 10  int 
+
+# float 1.5
+
+
+numberOfSTudents=30
+
+salary=20000.5
+
+
+# Boolean 
+
+isConnected=False
+
+
+
+
+Users=None
+
+
+
+
+
+
+
+
+
+
+# 
+
+
+numberOfClients=20000
+
+
+BankName="CIH"
+
+
+# AccountStatus=True
+
+
+# CreditAccordé=None
+
+
+# print(BankName)
+# print(AccountStatus)
+# print(CreditAccordé)
+
+# print(numberOfClients)
+
+# prenom="Sara"
+
+# nom="Alaoui"
+
+# age=29
+
+# profession="Software engineer"
+
+
+
+# print(prenom+" "+nom)
+# print("Hi my name is "+prenom+" "+nom+" , Im "+str(age)+" Years old  And i am a "+profession)
+
+
+
+name=input("Veuillez saisir votre nom complet ?")
+
+print("Bonjour "+name)
