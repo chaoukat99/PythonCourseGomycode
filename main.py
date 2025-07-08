0ec337d1b4493fc0ce3023ea286ee7409a8678a9
chaoukat99 <chawkatomar@gmail.com>
2025-07-08 13:20:40 +0100

    08/07/2025

--- main.py
+++ main.py
@@ -1,91 +1,88 @@
-#print("hello gomycoders")
-# this is a single line comment 
-#print("Hello developers")
-#print("hello marketers")
+# #print("hello gomycoders")
+# # this is a single line comment 
+# #print("Hello developers")
+# #print("hello marketers")
 
 
 
 
-# Uppercase
+# # Uppercase
 
-# Lowercase
+# # Lowercase
 
-# CamelCase
+# # CamelCase
 
-# Variables
-
-
-
-# Username="Samir"
+# # Variables
 
 
-# userIsConnected="Saleh"
+
+# # Username="Samir"
 
 
-#Type de données 
+# # userIsConnected="Saleh"
 
 
-# Opérateurs
+# #Type de données 
 
 
-# Control flow 
-   # conditions
-   # methodes Prédefinis  
-   # boucles 
-   # Functions
+# # Opérateurs
+
+
+# # Control flow 
+#    # conditions
+#    # methodes Prédefinis  
+#    # boucles 
+#    # Functions
   
 
 
 
 
 
-# Variables
+# # Variables
 
 
-# username="Omar"
+# # username="Omar"
 
-# username="AMine"
-# username="Samir"
-# print(username)
-
-
-
-# Data types
-
-# String (textes)
+# # username="AMine"
+# # username="Samir"
+# # print(username)
 
 
 
-email="email12@gmail.com"
-age=29
-city="Casablanca"
-test=False
+# # Data types
 
-print(email,age)
-
-# Numbers 
-
-# 10  int 
-
-# float 1.5
-
-
-numberOfSTudents=30
-
-salary=20000.5
-
-
-# Boolean 
-
-isConnected=False
+# # String (textes)
 
 
 
+# # email="email12@gmail.com"
+# # age=29
+# # city="Casablanca"
+# # test=False
 
-Users=None
+# # print(email,age)
+
+# # Numbers 
+
+# # 10  int 
+
+# # float 1.5
+
+
+# # numberOfSTudents=30
+
+# # salary=20000.5
+
+
+# # Boolean 
+
+# # isConnected=False
 
 
 
+
+# # Users=None
 
 
 
@@ -93,42 +90,363 @@ Users=None
 
 
 
+
+
+
+# # 
+
+
+# # numberOfClients=20000
+
+
+# # BankName="CIH"
+
+
+# # AccountStatus=True
+
+
+# # CreditAccordé=None
+
+
+# # print(BankName)
+# # print(AccountStatus)
+# # print(CreditAccordé)
+
+# # print(numberOfClients)
+
+# # prenom="Sara"
+
+# # nom="Alaoui"
+
+# # age=29
+
+# # profession="Software engineer"
+
+
+
+# # print(prenom+" "+nom)
+# # print("Hi my name is "+prenom+" "+nom+" , Im "+str(age)+" Years old  And i am a "+profession)
+
+
+
+# # name=input("Veuillez saisir votre nom complet ?")
+
+# # print("Bonjour "+name)
+
+
+# # schoolName="Hello \nguys"
+
+
+# # name=input("Veuillez saisir le nom")
+# # age=input("Veuillez saisir l'age")
+# # profession=input("Veuillez saisir la profession")
+# # status=input("Veuillez saisir le status")
+
+# # name="Hamid saleh"
+
+# # name=input("Veuillez saisir votre nom complet ?")
+
+# # print("FullName: "+name+"\nAge:"+age+"\nProfession:"+profession+"\nStatus:"+status)
+
+
+
+# # age=input("Veuillez saisir L'age ? ")
+
+# # print(type("hello"))
+# # print(type(20))
+# # print(type(20.6))
+# # print(type(False))
+# # print(type(None))
+
+
+
+
+
+# # print("FullName: "+name+"\nAge:"+str(age)+"\nProfession:"+profession+"\nStatus:"+str(status))
+
+
+
+# # departement=input("veuillez saisir une phrase : ")
+
+
+
+# # print(len(departement))
+
+
+
+# # Opérateurs : arith , comparaison , logique
+
+
+# # Arith
+
+
+# number1=10
+
+# number2=20
+
+
+# # Addition
+
+# print(number1+number2)
+# # Soustraction
+
+# print(number1-number2)
+# # multiplation
+
+# print(number1*number2)
+# # Divison
+
+
+# print(float(12))
+
+# # La division
+
+# # Modulo
+
+# # print(number1%number2)
+
+
+# # programme 
+# # L'addition de 10+20 = 30
+
+
+
+
+# num1= int( input("Veullez Saisir le  Nombre 1 : ")) # str 
+# num2= int( input("Veullez Saisir le  Nombre 2 : ")) # str 
+
+
+# # Traitement 
+
+
+# # Addition 
+# addition=num1+num2
+# print("L'addition de "+str(num1)+"+"+str(num2)+" est : "+str(addition))
+# # Soustraction
+# sous=num1-num2
+
+# print("La soustraction de "+str(num1)+"-"+str(num2)+" est : "+str(sous))
+
+# # Multiplication
+
+# # multi=num1*num2
+# # print("La multiplication de "+str(num1)+"x"+str(num2)+" est : "+str(multi))
+
+# # Division 
+
+# # division=num1/num2
+
+# # print("La division de "+str(num1)+"/"+str(num2)+" est : "+str(multi))
+
+
+
+
+
+
+# Opérateur de comparaison
+
+
+# < > <= >= ==  != 
+
+
+# number1=20
+
+# number2=21
+
+
+# print(number1!=number2)
+
+
+
+# user="Karim"
+# password="karim123"
+
+# # Operateur logique
+# # and
+
+# # print(user=="Karim15" and  password=="salim123")
+
+  
+# # True and True = True
+# # True and False = False 
+# # False and True = False
+# # False and False = False 
+
+
+
+# user="Karim"
+# password="karim123"
+
+# # or  
+# print(user=="Karimm" or password=="karim1g23" or len("hello")==5 )
+
+
+#  True or True = True 
+
+#  True or False = True 
+
+# false or True = True 
+
+# false or false = False
+
+
+
+
+
+
+# print(a+d>c*2 or "hello"=="Hello" or b-4-1 == len("")  )
+
+
+# a=10
+# b=a-5
+# c=b+10
+# d=c+b
+# e=c
+# c=10
+
+
+# print((a*2)%(18+len("hi"))==0) and (c==age or age==(a+2)) and not (age>10 and age <12 ) 
+
+
+
+
+# chaine="hello"
+# a=4
+# b=a-3
+# c=a*2+2
+# f=c%20
+# print( not (
 # 
+# (a+1)!=len(chaine)) and len(chaine)*2 == c and c+2>=10 and c+2<11 )
+
+# print( not (c%(c*2) == c ) and (len("test") != len(chaine)-1 ) or not (f<=6 and f>=11)     )
 
 
-numberOfClients=20000
+# Control Flow 
 
 
-BankName="CIH"
+#   Condition 
 
 
-# AccountStatus=True
+age=12
+
+# if condition:
+    #   traitement
+
+#DRY
+
+# age >18
+# age ==17 
+# age <18 and age <17 
 
 
-# CreditAccordé=None
+if age>18:
+   print("Vous etes Majeur")
+elif age==17:
+   print("Vous avez 17 ans ")   
+elif age == 16:
+   print("Vous avez 16 ans ")   
+else:
+   print("Vous etes Mineur ")    
 
+# if age<17:
+#    print("Vous etes mineur")
 
-# print(BankName)
-# print(AccountStatus)
-# print(CreditAccordé)
-
-# print(numberOfClients)
-
-# prenom="Sara"
-
-# nom="Alaoui"
-
-# age=29
-
-# profession="Software engineer"
-
-
-
-# print(prenom+" "+nom)
-# print("Hi my name is "+prenom+" "+nom+" , Im "+str(age)+" Years old  And i am a "+profession)
+# if age==12:
+#    print("Votre age est  12")
+  
 
 
 
-name=input("Veuillez saisir votre nom complet ?")
+# un programme qui va demander a l'utilisateur de
+# saisir un nombre et en se basant sur le nombre il faut afficher le jour 
+# if age>18:
+#    print("Vous etes Majeur")
+# elif age==17:
+#    print("Vous avez 17 ans ")   
+# elif age == 16:
+#    print("Vous avez 16 ans ")   
+# else:
+#    print("Vous etes Mineur ")  
 
-print("Bonjour "+name)
+
+
+
+# day= int(input("Veullez Saisir Une valeur ")) # Demander a l'utilisateur 
+
+
+
+# if day==1:
+#    print(" Lundi")
+# elif day==2:
+#    print("Mardi")   
+# elif day == 3:
+#    print("Mercredi ")   
+# elif day == 4 : 
+#    print("Jeudi")   
+# elif day ==5 :
+#    print("Vendredi")
+# elif day ==6 :
+#    print("Samedi")   
+# elif day == 7 :
+#    print("Dimanche")   
+# else :
+#    print("valeur non valide ")   
+
+
+
+
+
+
+
+# Password Saisi par l'utilisateur doit etre entre 8 et 15 caractére 
+
+# le programme doit afficher Mot de passe valide / non valide 
+
+
+
+password=input("Mot de passe : ")
+
+# if len(password)>=8 and len(password)<=15:
+#    print("Mot de pass valide ")
+# else:
+#    print("Mot de passe Invalide")
+   
+
+
+
+
+# Créer Un programme Qui va demander 
+# a l'utilisateur de saisir un nombre 
+# et le programme va afficher le nombre est paire | Impaire
+
+# La condition : num%2 == 0 (nb : num c'est la valeur saisi par User )
+
+
+# number= int(input("Veuillez Saisir une valeur Numerique : "))
+
+
+# if number%2==0:
+#    print
+   
+
+
+# else:
+#    print("Impair")   
+
+
+
+
+print("1-Addition\n2-Soustraction\n3-Multiplication\n4-Division")
+
+
+# le programme il va demander a l'utilisateur 
+
+# Saisir le 1st number
+# Saisir le 2nd number
+# saisir Option (1-4)
+# Afficher le resultas  suivante :
+# Le resultat de l'addition de 4+5 est : 9
+
+
+
